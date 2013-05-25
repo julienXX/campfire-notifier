f118bc1d30ff3511aba280aa88faf10840fc0da9
Julien Blanchard <julien@sideburns.eu>
2013-05-25 15:56:01 +0200

    Update Clojure version and libs

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main campfire.core
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [http.async.client "0.5.0"]
-                 [cheshire "5.0.1"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [http.async.client "0.5.2"]
+                 [cheshire "5.2.0"]
                  [clj-growl "0.2.1"]])
